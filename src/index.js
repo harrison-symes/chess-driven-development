f61ececd93f770a80cecf936f520aa5e1dc03c33
harrison-symes <symeshjb@gmail.com>
2017-05-18 15:00:19 +1200

    moved client to a src folder, working on pages

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
 
-render(<App name='CDD' />, document.querySelector('main'))
+render(<App name='CDD' />, document.getElementById('app'))
 console.log('welcome to CDD')
